6a7bb589fa723cc893668789ed2e17a9e922b377
aayush.koi <aayush.k204@gmail.com>
2024-07-24 21:35:30 -0400

    trying to fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+  
+base: '/Personal-Portfolio/',
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Personal-Portfolio/',
   plugins: [react()],
 })
